9216bdc0542ffc234a2d13d4fe30bf9cbb304d43
matthew rendely <matthew.rendely@gmail.com>
2023-08-26 10:55:10 -0700

    First prop and interface created

--- src/App.tsx
+++ src/App.tsx
@@ -1,59 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import AspectRatio from '@mui/joy/AspectRatio';
-import Button from '@mui/joy/Button';
-import Card from '@mui/joy/Card';
-import CardContent from '@mui/joy/CardContent';
-import IconButton from '@mui/joy/IconButton';
-import Typography from '@mui/joy/Typography';
+import SnippetCard from './SnippetCard'
 
-function App() {
+export default function App() {
+  const data = {
+    title: 'Title',
+    date: 'Aug 26, 2023'
+  }
   return (
     <div className="App">
-      <header className="App-header">      
-      <Card variant="outlined" sx={{ width: 320 }}>
-      <div>
-        <Typography level="title-lg">Yosemite National Park</Typography>
-        <Typography level="body-sm">April 24 to May 02, 2021</Typography>
-        <IconButton
-          aria-label="bookmark Bahamas Islands"
-          variant="plain"
-          color="neutral"
-          size="sm"
-          sx={{ position: 'absolute', top: '0.875rem', right: '0.5rem' }}
-        >
-        </IconButton>
-      </div>
-      <AspectRatio minHeight="120px" maxHeight="200px">
-        <img
-          src="https://images.unsplash.com/photo-1527549993586-dff825b37782?auto=format&fit=crop&w=286"
-          srcSet="https://images.unsplash.com/photo-1527549993586-dff825b37782?auto=format&fit=crop&w=286&dpr=2 2x"
-          loading="lazy"
-          alt=""
-        />
-      </AspectRatio>
-      <CardContent orientation="horizontal">
-        <div>
-          <Typography level="body-xs">Total price:</Typography>
-          <Typography fontSize="lg" fontWeight="lg">
-            $2,900
-          </Typography>
-        </div>
-        <Button
-          variant="solid"
-          size="md"
-          color="primary"
-          aria-label="Explore Bahamas Islands"
-          sx={{ ml: 'auto', alignSelf: 'center', fontWeight: 600 }}
-        >
-          Explore
-        </Button>
-      </CardContent>
-    </Card>
-    </header>
+      <SnippetCard data={data} />
     </div>
   );
 }
-
-export default App;

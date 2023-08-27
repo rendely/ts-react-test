c450c6a4d9fbf6a4cdb02717319f6e778eef0ec0
matthew rendely <matthew.rendely@gmail.com>
2023-08-27 16:35:28 -0700

    Setup folder structure for server

--- server/app.py
+++ server/app.py
@@ -1,23 +1,7 @@
 #!/usr/bin/env python3
-import sys
-import os
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-from datetime import datetime
-
-from flask import render_template
-
 from config import app
+from routes.routes import *
 # from server.models.models import *
-# from server.routes.routes import *
-
-@app.route('/')
-def index():
-    return render_template("index.html")
-
-@app.errorhandler(404)
-def not_found(e):
-    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
---
+++ server/routes/index.py
@@ -0,0 +1,10 @@
+from flask import render_template
+from config import app
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.errorhandler(404)
+def not_found(e):
+    return render_template("index.html")

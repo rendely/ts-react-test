10f1dddbd9105aaf8426bb52eae19ffb4567192b
matthew rendely <matthew.rendely@gmail.com>
2023-08-28 13:25:12 -0700

    Add 1000 fake sample snippets and do simple keyword search in App component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,30 @@
 import { useEffect, useState } from 'react'
 import SnippetCard from './components/SnippetCard'
-import {SnippetType} from './types'
+import { SnippetType } from './types'
 
 export default function App() {
   const [snippets, setSnippets] = useState<SnippetType[]>([])
+  const [filter, setFilter] = useState<string>('')
 
   useEffect(() => {
     fetch('/api/snippets')
-    .then(r => r.json())
-    .then(d => setSnippets(d))
-  },[])
+      .then(r => r.json())
+      .then(d => setSnippets(d))
+  }, [])
 
+  const filteredSnippets = snippets.filter(s => s.content.match(filter))
   return (
     <div className="App">
-      {snippets.map(s => <SnippetCard key={s.id} snippet={s} />)}
-      
+      <form>
+        <label>Search:</label>
+        {filteredSnippets.length} / {snippets.length} match
+        <br></br>
+        <input onChange={(e: React.ChangeEvent<HTMLInputElement>) => setFilter(e.currentTarget.value)}
+          type='text'
+          value={filter}>
+        </input>
+      </form>
+      {filteredSnippets.map(s => <SnippetCard key={s.id} snippet={s} />)}
     </div>
   );
 }
--- server/seed.py
+++ server/seed.py
@@ -1,12 +1,13 @@
 from app import app, db
 from models.models import *
+from faker import Faker
+fake = Faker()
 
 with app.app_context():
     print("Starting seed...")
     Snippet.query.delete()
-    snippet = Snippet(content='This is how you sort an array')
-    db.session.add(snippet)
-    snippet = Snippet(content='This is how you filter an array')
-    db.session.add(snippet)
+    for _ in range(1000):
+        snippet = Snippet(content=fake.text())
+        db.session.add(snippet)
     db.session.commit()
     print("Finished seed!")

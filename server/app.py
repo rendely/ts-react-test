6ac71fd1bbe1fcd658d0b50deea19a19da123071
matthew rendely <matthew.rendely@gmail.com>
2023-08-27 17:30:45 -0700

    remove all server code

--- server/app.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-from config import app
-from routes.routes import *
-# from server.models.models import *
-
-if __name__ == '__main__':
-    app.run(port=5555, debug=True)
--- server/debug.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-from models.models import Snippet
-from config import app, db
-
-if __name__ == '__main__':
-    
-    with app.app_context():
-        import ipdb; ipdb.set_trace()

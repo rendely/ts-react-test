4b1292c0c5d2efda496ed9d9fb8747272397ef0c
Sofia Katzman <katzmansof@gmail.com>
2023-08-29 16:40:10 -0400

    new user handling function passed as props

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,15 @@
 import { useEffect, useState } from 'react'
 import SnippetCard from './components/SnippetCard'
-import { SnippetType } from './types'
+import { SnippetType, UserType } from './types'
 
 import TestForm from './components/TestForm'
+import { text } from 'stream/consumers'
 
 export default function App() {
   const [snippets, setSnippets] = useState<SnippetType[]>([])
   const [filter, setFilter] = useState<string>('')
+  const [users, setUsers] = useState<UserType[]>([])
+
 
   useEffect(() => {
     fetch('/api/snippets')
@@ -14,11 +17,17 @@ export default function App() {
       .then(d => setSnippets(d))
   }, [])
 
+  //Adds newly added user to stateful users object
+  const handleAddNewUser = (name: string, email: string ) => {
+    const newUser = { id: Math.random(), name: name, email:email}
+    setUsers((prevUsers) => [...prevUsers, newUser])
+    
+  }
+  console.log(users)
   const filteredSnippets = snippets.filter(s => s.content.match(filter))
   return (
     <div className="App">
-  
-      <TestForm/>
+      <TestForm addUser={handleAddNewUser}/>
       <form>
         <label>Search:</label>
         {filteredSnippets.length} / {snippets.length} match
